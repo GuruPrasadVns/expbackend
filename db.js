a7f9659b975d8a3a539c8ecae493471e8c89e866
prasag1 <guru.prasad@bsci.com>
2024-05-07 17:38:02 +0530

    db connection removed

--- db.js
+++ db.js
@@ -1,16 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connection = mongoose.connection;
 
-async function connectToMongoDBAtlas(){
-    try{
-       //await mongoose.connect(`mongodb+srv://prasag1admin:prasag1admin@cluster0.ny9e5y6.mongodb.net/exptrackerbackend`);
-        await mongoose.connect(`mongodb://localhost:27017/expensebackend`);
-        //
-        console.log(`connected to mongodb atlas.....`);
-    }catch (e) {
-        console.log(`Error in connecting mongodb atlas ....${e.message}`)
-    }
+async function connectToMongoDBAtlas() {
+  try {
+    await mongoose.connect(``);
+    //
+    console.log(`connected to mongodb atlas.....`);
+  } catch (e) {
+    console.log(`Error in connecting mongodb atlas ....${e.message}`);
+  }
 }
 
 module.exports.connectToMongoDBAtlas = connectToMongoDBAtlas;
